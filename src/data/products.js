f2256d44961ca364cc028aefae3c3f1835d4c72d
Kristina <kristina22011998@gmail.com>
2026-03-14 22:48:29 +0200

    Added Rockstar games, edit, resize

--- src/data/products.js
+++ src/data/products.js
@@ -107,354 +107,83 @@ export const products = [
 
   {
     id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
-    image: "images/products/women-sandal-heels-white-pink.jpg",
-    name: "Women's Sandal Heels - Pink",
+    image: "images/products/gta sa.jpg",
+    name: "Grand Theft Auto: San Andreas",
     rating: {
-      stars: 4.5,
-      count: 2286
+      stars: 5,
+      count: 1270
     },
-    priceCents: 5300,
-    keywords: ["garbage", "bins", "cans", "kitchen"]
+    priceCents: 5999,
+    keywords: ["gtasanandreas", "san andreas", "gta", "trilogy", "grand theft auto"]
   },
   {
     id: "aad29d11-ea98-41ee-9285-b916638cac4a",
-    image: "images/products/round-sunglasses-gold.jpg",
-    name: "Round Sunglasses",
+    image: "images/products/GTA IV.jpg",
+    name: "GTA IV",
     rating: {
-      stars: 4.5,
-      count: 30
+      stars: 4.0,
+      count: 430
     },
-    priceCents: 3560,
-    keywords: ["accessories", "shades"]
+    priceCents: 1999,
+    keywords: ["gta 4", "grand theft auto"]
   },
   {
     id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
-    image: "images/products/blackout-curtain-set-beige.jpg",
-    name: "Blackout Curtains Set - Beige",
+    image: "images/products/GTA V.jpg",
+    name: "Grand Theft Auto V",
     rating: {
       stars: 4.5,
-      count: 232
+      count: 2232
     },
-    priceCents: 4599,
-    keywords: ["bedroom", "curtains", "home"]
+    priceCents: 2999,
+    keywords: ["gta5", "grand theft auto 5"]
   },
   {
     id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
-    image: "images/products/women-summer-jean-shorts.jpg",
-    name: "Women's Summer Jean Shorts",
+    image: "images/products/GTA 6.jpg",
+    name: "GTA VI Coming Soon",
     rating: {
-      stars: 4,
-      count: 160
+      stars: 0,
+      count: 0
     },
-    priceCents: 1699,
-    keywords: ["shorts", "apparel", "mens"]
+    priceCents: 9999,
+    keywords: ["gta 6", "grand theft auto"]
   },
   {
     id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
-    image: "images/products/electric-steel-hot-water-kettle-white.jpg",
-    name: "Electric Hot Water Kettle - White",
+    image: "images/products/RDR1.jpg",
+    name: "Red Dead Redemption",
     rating: {
-      stars: 5,
+      stars: 4,
       count: 846
     },
-    priceCents: 5074,
-    keywords: ["water kettle", "appliances", "kitchen"]
+    priceCents: 4999,
+    keywords: ["game", "rdr1", "red dead redemption"]
   },
   {
     id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
-    image: "images/products/knit-athletic-sneakers-gray.jpg",
-    name: "Waterproof Knit Athletic Sneakers - Gray",
-    rating: {
-      stars: 4,
-      count: 89
-    },
-    priceCents: 5390,
-    keywords: ["shoes", "running shoes", "footwear"]
-  },
-  {
-    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
-    image: "images/products/straw-sunhat.jpg",
-    name: "Straw Wide Brim Sun Hat",
-    rating: {
-      stars: 4,
-      count: 215
-    },
-    priceCents: 2200,
-    keywords: ["hats", "straw hats", "summer", "apparel"]
-  },
-  {
-    id: "1c079479-8586-494f-ab53-219325432536",
-    image: "images/products/men-athletic-shoes-white.jpg",
-    name: "Men's Athletic Sneaker - White",
-    rating: {
-      stars: 4,
-      count: 229
-    },
-    priceCents: 4590,
-    keywords: ["shoes", "running shoes", "footwear", "mens"]
-  },
-  {
-    id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
-    image: "images/products/men-stretch-wool-sweater-black.jpg",
-    name: "Men's Wool Sweater - Black",
-    rating: {
-      stars: 4.5,
-      count: 2465
-    },
-    priceCents: 3374,
-    keywords: ["sweaters", "apparel"]
-  },
-  {
-    id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
-    image: "images/products/bathroom-mat.jpg",
-    name: "Bathroom Bath Mat 16 x 32 Inch - Grey",
-    rating: {
-      stars: 4.5,
-      count: 119
-    },
-    priceCents: 1850,
-    keywords: ["bathmat", "bathroom", "home"]
-  },
-  {
-    id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
-    image: "images/products/women-knit-ballet-flat-white.jpg",
-    name: "Women's Ballet Flat - White",
-    rating: {
-      stars: 4,
-      count: 326
-    },
-    priceCents: 2640,
-    keywords: ["shoes", "flats", "womens", "footwear"]
-  },
-  {
-    id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
-    image: "images/products/men-golf-polo-t-shirt-gray.jpg",
-    name: "Men's Golf Polo Shirt - Gray",
-    rating: {
-      stars: 4.5,
-      count: 2556
-    },
-    priceCents: 1599,
-    keywords: ["tshirts", "shirts", "apparel", "mens"]
-  },
-  {
-    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
-    image: "images/products/laundry-detergent-tabs.jpg",
-    name: "Laundry Detergent Tabs, 50 Loads",
-    rating: {
-      stars: 4.5,
-      count: 305
-    },
-    priceCents: 2899,
-    keywords: ["bathroom", "cleaning"]
-  },
-  {
-    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
-    image: "images/products/sky-leaf-branch-earrings.jpg",
-    name: "Sterling Silver Leaf Branch Earrings",
-    rating: {
-      stars: 4.5,
-      count: 52
-    },
-    priceCents: 6799,
-    keywords: ["jewelry", "accessories", "womens"]
-  },
-  {
-    id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
-    image: "images/products/duvet-cover-set-gray-queen.jpg",
-    name: "Duvet Cover Set, Diamond Pattern",
-    rating: {
-      stars: 4,
-      count: 456
-    },
-    priceCents: 4399,
-    keywords: ["bedroom", "bed sheets", "sheets", "covers", "home"]
-  },
-  {
-    id: "d2785924-743d-49b3-8f03-ec258e640503",
-    image: "images/products/women-knit-beanie-pom-pom-blue.jpg",
-    name: "Women's Knit Winter Beanie - Blue",
+    image: "images/products/RDR2.png",
+    name: "Red Dead Redemption 2",
     rating: {
       stars: 5,
-      count: 83
+      count: 1971
     },
-    priceCents: 1950,
-    keywords: ["hats", "winter hats", "beanies", "apparel", "womens"]
+    priceCents: 5999,
+    keywords: ["game", "rdr2", "red dead redemption II", "red dead redemption 2"]
   },
-  {
-    id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
-    image: "images/products/men-chino-pants-beige.jpg",
-    name: "Men's Chino Pants - Beige",
-    rating: {
-      stars: 4.5,
-      count: 9017
-    },
-    priceCents: 2290,
-    keywords: ["pants", "apparel", "mens"]
-  },
-  {
-    id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
-    image: "images/products/men-navigator-sunglasses-black.jpg",
-    name: "Men's Navigator Sunglasses",
-    rating: {
-      stars: 3.5,
-      count: 42
-    },
-    priceCents: 3690,
-    keywords: ["sunglasses", "glasses", "accessories", "shades"]
-  },
-  {
-    id: "04701903-bc79-49c6-bc11-1af7e3651358",
-    image: "images/products/men-brown-flat-sneakers.jpg",
-    name: "Men's Brown Flat Sneakers",
-    rating: {
-      stars: 4.5,
-      count: 562
-    },
-    priceCents: 2499,
-    keywords: ["footwear", "men", "sneakers"]
-  },
-  {
-    id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
-    image: "images/products/non-stick-cooking-set-4-pieces.jpg",
-    name: "Non-Stick Cook Set With Lids - 4 Pieces",
-    rating: {
-      stars: 4.5,
-      count: 511
-    },
-    priceCents: 6797,
-    keywords: ["cooking set", "kitchen"]
-  },
-  {
-    id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
-    image: "images/products/vanity-mirror-pink.jpg",
-    name: "Vanity Mirror with LED Lights - Pink",
-    rating: {
-      stars: 4.5,
-      count: 130
-    },
-    priceCents: 2549,
-    keywords: ["bathroom", "washroom", "mirrors", "home"]
-  },
-  {
-    id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
-    image: "images/products/women-relaxed-lounge-pants-pink.jpg",
-    name: "Women's Relaxed Lounge Pants - Pink",
-    rating: {
-      stars: 4.5,
-      count: 248
-    },
-    priceCents: 3400,
-    keywords: ["pants", "pants", "apparel", "womens"]
-  },
-  {
-    id: "d339adf3-e004-4c20-a120-40e8874c66cb",
-    image: "images/products/crystal-zirconia-stud-earrings-pink.jpg",
-    name: "Crystal Zirconia Stud Earrings - Pink",
-    rating: {
-      stars: 4.5,
-      count: 117
-    },
-    priceCents: 3467,
-    keywords: ["accessories", "womens"]
-  },
-  {
-    id: "d37a651a-d501-483b-aae6-a9659b0757a0",
-    image: "images/products/glass-screw-lid-food-containers.jpg",
-    name: "Glass Screw Lid Containers - 3 Pieces",
-    rating: {
-      stars: 4,
-      count: 126
-    },
-    priceCents: 2899,
-    keywords: ["food containers", "kitchen"]
-  },
-  {
-    id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
-    image: "images/products/black-and-silver-espresso-maker.jpg",
-    name: "Black and Silver Espresso Maker",
-    rating: {
-      stars: 4.5,
-      count: 1211
-    },
-    priceCents: 8250,
-    keywords: ["espresso makers", "kitchen", "appliances"]
-  },
-  {
-    id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
-    image: "images/products/blackout-curtains-set-teal.jpg",
-    name: "Blackout Curtains Set 42 x 84-Inch - Teal",
-    rating: {
-      stars: 4.5,
-      count: 363
-    },
-    priceCents: 3099,
-    keywords: ["bedroom", "home", "curtains"]
-  },
-  {
-    id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
-    image: "images/products/bath-towel-set-gray-rosewood.jpg",
-    name: "Bath Towels 2 Pack - Gray, Rosewood",
-    rating: {
-      stars: 4.5,
-      count: 93
-    },
-    priceCents: 2990,
-    keywords: ["bathroom", "home", "towels"]
-  },
-  {
-    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
-    image: "images/products/athletic-skateboard-shoes-gray.jpg",
-    name: "Athletic Skateboard Shoes - Gray",
-    rating: {
-      stars: 4,
-      count: 89
-    },
-    priceCents: 3390,
-    keywords: ["shoes", "running shoes", "footwear"]
-  },
-  {
-    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
-    image: "images/products/countertop-push-blender-black.jpg",
-    name: "Countertop Push Blender - Black",
-    rating: {
-      stars: 4,
-      count: 3
-    },
-    priceCents: 10747,
-    keywords: ["food blenders", "kitchen", "appliances"]
-  },
-  {
-    id: "bc2847e9-5323-403f-b7cf-57fde044a955",
-    image: "images/products/men-cozy-fleece-hoodie-light-teal.jpg",
-    name: "Men's Fleece Hoodie - Light Teal",
-    rating: {
-      stars: 4.5,
-      count: 3157
-    },
-    priceCents: 3800,
-    keywords: ["sweaters", "hoodies", "apparel", "mens"]
-  },
-  {
-    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
-    image: "images/products/artistic-bowl-set-6-piece.jpg",
-    name: "Artistic Bowl and Plate Set - 6 Pieces",
-    rating: {
-      stars: 5,
-      count: 679
-    },
-    priceCents: 3899,
-    keywords: ["bowls set", "kitchen"]
-  },
-  {
-    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
-    image: "images/products/kitchen-paper-towels-8-pack.jpg",
-    name: "2-Ply Kitchen Paper Towels - 8 Pack",
-    rating: {
-      stars: 4.5,
-      count: 1045
-    },
-    priceCents: 1899,
-    keywords: ["kitchen", "kitchen towels", "tissues"]
-  }
+
+  //  -----UBISOFT------
+  
+//  {
+//     id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+//     image: "images/products/RDR2.jpg",
+//     name: "Red Dead Redemption II",
+//     rating: {
+//       stars: 5,
+//       count: 1971
+//     },
+//     priceCents: 5999,
+//     keywords: ["game", "rdr2", "red dead redemption II", "red dead redemption 2"]
+//   },
+
 ];
